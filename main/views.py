c78fbfd6a0baee0b4fd375fd0b8b10a2a697a197
Conor Sliide <conor@Conors-MacBook-Pro.local>
2020-05-18 18:00:29 +0100

    Separating pages out for loading

--- main/views.py
+++ main/views.py
@@ -59,12 +59,45 @@ class GenerateExport(FormView):
     def form_valid(self, form):
         job_id = str(uuid.uuid4())
         django_rq.enqueue(generate_file, form.files['file'], job_id=job_id)
-        return redirect(f'/?j={job_id}')
+        return redirect(f'/success?j={job_id}')
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(job_status=self.job_status, **kwargs)
 
 
+class GenerateExportPro(FormView):
+    template_name = 'index2.html'
+    form_class = NameSubmitForm
+    success_url = reverse_lazy('processing')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.job_status = ''
+
+    def dispatch(self, request, *args, **kwargs):
+        job_id = self.request.GET.get('j')
+        if job_id:
+            job = Job.fetch(job_id, connection=redis_conn)
+            if job.get_status() == 'ongoing':
+                # Show loading
+                self.job_status = 'ongoing'
+                pass
+            elif job.get_status() == 'finished':
+                streamed_workbook, filename = job.return_value
+                response = HttpResponse(content=streamed_workbook, content_type='application/ms-excel')
+                response['Content-Disposition'] = "attachment; filename={}".format(filename)
+                return response
+            # elif job.get_status() == TrackedJobStatus.FAILED:
+            #    print("Why though?")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        job_id = str(uuid.uuid4())
+        django_rq.enqueue(generate_file, form.files['file'], job_id=job_id)
+        return redirect(f'/processing?j={job_id}')
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(job_status=self.job_status, **kwargs)
 
 # class Dashboard(FormView):
 #     form_class = NameSubmitForm
@@ -83,4 +116,5 @@ class GenerateExport(FormView):
 #
 
 dashboard = GenerateExport.as_view()
+dashboard = GenerateExportPro.as_view()
 
